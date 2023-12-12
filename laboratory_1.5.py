be70f9f3977efa0c3f266bc48e77874ede0a9aa6
Ignat <60806016+ignvt@users.noreply.github.com>
2023-12-13 04:11:22 +0500

    feat: migrate to requests & BeautifulSoup

--- laboratory_1.5.py
+++ laboratory_1.5.py
@@ -1,17 +1,41 @@
-import wikipedia
+import requests
 import json
+from bs4 import BeautifulSoup
 
-search = "Ежик в тумане"
-response = wikipedia.search(search)
-main_content = wikipedia.page(response[0])
+search = "Ёжик в тумане"
+urlAPI = "https://ru.wikipedia.org/w/api.php"
+domain = "https://ru.wikipedia.org"
 
-data_to_json = {
-    "href": main_content.url,
-    "mainTitle": main_content.title,
-    "content": main_content.content,
-    "images": main_content.images,
-    "links": main_content.links,
-    "references": main_content.references
-}
-with open("data.json", "w") as f:
-    json.dump(data_to_json, f)
+
+def get_json(search_page):
+    params = {
+        "action": "parse",
+        "format": "json",
+        "page": search_page,
+        "prop": "text|images|links"
+    }
+    response = requests.get(urlAPI, params=params)
+    response_json = response.json()
+    if "error" in response:
+        print("error: missing title")
+    else:
+        html = response_json["parse"]["text"]["*"]
+        soup = BeautifulSoup(html, "html.parser")
+        links = []
+        references = soup.find("ol", {"class": "references"}).get_text()
+        for link in response_json["parse"]["links"]:
+            if link["ns"] == 0:
+                links.append(domain + "/wiki/" + link["*"])
+        data_to_json = {
+            "href": response.request.url,
+            "mainTitle": response_json["parse"]["title"],
+            "content": response_json["parse"]["text"]["*"],
+            "images": response_json["parse"]["images"],
+            "links": links,
+            "references": references
+        }
+        with open("data.json", "w", encoding="utf-8") as f:
+            json.dump(data_to_json, f, ensure_ascii=False)
+
+
+get_json(search)
